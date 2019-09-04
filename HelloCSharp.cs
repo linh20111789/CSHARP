5bbf9dda0d26b9ea00130c344569fa4e87f598c3
linh20111789 <linh20111789@gmail.com>
2019-09-04 18:19:15 +0900

    Add files via upload

---
+++ HelloCSharp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_CSHARP
+{
+    class HelloCSharp
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello Linh-san");
+        //    Console.ReadLine();
+        
+        //}
+    }
+}
