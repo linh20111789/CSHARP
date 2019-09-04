1c2b86582a13895bf96155bc5db9e03761f576da
linh20111789 <linh20111789@gmail.com>
2019-09-04 18:20:17 +0900

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolymorphism
+{
+    public abstract class Animal
+    {
+        public string NameAnimal;
+
+        // Default constructor
+        public Animal()
+        {
+            Console.WriteLine("Animal");
+        }
+
+        public Animal(string sName)
+        {
+            this.NameAnimal = sName;
+            Console.WriteLine("Print Name: ###");
+        }
+
+        // virtual for Polymorphism of child class
+        public virtual void Move()
+        {
+            Console.WriteLine(">>>Move<<<");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(">>Sleep<<");
+        }
+
+    }
+}
---
+++ MyClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace o7planning.CSharp
+{
+    class MyClass
+    {
+
+        static void Main(string[] args)
+        {
+            int i32Var4Switch = 10;
+            int i32Var4For = 10;
+            int i32Var4If = 10;
+
+
+            Console.WriteLine("Linh Saru");
+            Console.ReadLine();
+
+            switch (i32Var4Switch)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    break;
+
+                case 9:
+                case 10:
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (i32Var4If == 100)
+            {
+                Console.WriteLine("i32Var4If = 100");
+                Console.WriteLine("Exit");
+            }
+
+            for (i32Var4For =0; i32Var4For < 10; i32Var4For++)
+            {
+                Console.WriteLine("######");
+            }   
+        }
+    }
+}
